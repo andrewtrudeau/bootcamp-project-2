b118c0ca42eb0c85d0e0d45825320456ee77614e
Preston <prestonconley5@gmail.com>
2021-11-01 14:13:49 -0500

    made models for artwork, comments, user, and image posts

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        comment_text:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+)
